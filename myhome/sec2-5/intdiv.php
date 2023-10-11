7c8dbafe10ec8171da55333e889d4e074be40a76
SeiichiN <billie175@gmail.com>
2023-10-11 09:25:52 +0900

    intdiv.php modified.

--- myhome/sec2-5/intdiv.php
+++ myhome/sec2-5/intdiv.php
@@ -3,8 +3,8 @@ $a = 9;
 $b = 4;
 try {
   $c = intdiv($a, $b);
-  $kotae = $c;
+  $ans = $c;
 } catch (DivisionByZeroError $e) {
-  $kotae = "0で割るのは無理";
+  $ans = "0で割るのは無理";
 }
-echo $kotae, PHP_EOL;
+echo $ans, PHP_EOL;
