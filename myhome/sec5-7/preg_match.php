89d667ec608501fe3bfc873b6f27ab49be9b3d49
SeiichiN <billie175@gmail.com>
2023-10-13 21:47:23 +0900

    sec5-8 added.

--- myhome/sec5-7/preg_match.php
+++ myhome/sec5-7/preg_match.php
@@ -1,9 +1,28 @@
 <?php
-$text = "たぶん48-49でした";
-$pattern = "/4[6-9]-49/u";
+$text = "たぶん42-49でした";
+$pattern = "/4[6-9]-49u";
 
-if (preg_match($pattern, $text)) {
+$result = preg_match($pattern, $text);
+
+// 文法エラーも含めてチェック
+if ($result === 1) {
   echo "マッチしました。";
-} else {
+} else if ($result === 0){
   echo "マッチしませんでした。";
+} else {
+  echo "エラーです。";
 }
+
+
+echo "<br>\n";
+
+// 単なる正規表現チェック(これでよい)
+if ($result) {
+  echo "2:マッチしました。";
+} else {
+  echo "2:マッチしませんでした。";
+}
+
+
+
+// 修正時刻: Fri 2023/10/13 16:29:35
